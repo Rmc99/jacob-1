16f2098f2d2edb33b35535440c3ebd9b36076920
Ronaldson Mendes de Castro <ronaldson.mendes@gmail.com>
2024-04-19 22:25:58 -0300

    Comentários

--- principal.py
+++ principal.py
@@ -1,34 +1,33 @@
 import requests
-#from collections import deque
 
-class Grafo:
-    def __init__(self, nome_arquivo):
+class Grafo:    # Classe grafo onde será criado os métodos padrões
+    def __init__(self, nome_arquivo):   # Inicializamos a classe
         self.nome_arquivo = nome_arquivo
         self.matriz_adjacencia = []
         self.vertices = []
         self.arestas = []
         self.tipo_grafo = ""
 
-    def ler_arquivo_padrao(self):
+    def ler_arquivo_padrao(self): # Método que lê o arquivo
         with open(self.nome_arquivo, 'r') as arquivo:
-            self.tipo_grafo = arquivo.readline().upper().strip()
-            self.arestas = [linha.strip().split(',') for linha in arquivo]
+            self.tipo_grafo = arquivo.readline().upper().strip()    # Lê a 1º linha converte para maiusculo e remove espaços
+            self.arestas = [linha.strip().split(',') for linha in arquivo]  # Lê o restante das linhas
             self.vertices = sorted(set(v for aresta in self.arestas for v in aresta))
 
-    def gerar_matriz_adjacente(self):
-        self.matriz_adjacencia = [[0] * len(self.vertices) for _ in range(len(self.vertices))]
-        for v1, v2 in self.arestas:
+    def gerar_matriz_adjacente(self):   # Gera a matriz de adjacência
+        self.matriz_adjacencia = [[0] * len(self.vertices) for _ in range(len(self.vertices))]  # Gera sub listas com o qtd de vértices
+        for v1, v2 in self.arestas: # Aqui ajustamos a indexação baseada em zero do python
             indice_v1 = self.vertices.index(v1)
             indice_v2 = self.vertices.index(v2)
             self.matriz_adjacencia[indice_v1][indice_v2] = 1
 
-            if self.tipo_grafo == 'ND':
+            if self.tipo_grafo == 'ND':     # Se o grafo for não direcionado colocamos o 1 em ambas as direções
                 self.matriz_adjacencia[indice_v2][indice_v1] = 1
             elif self.tipo_grafo == "D":
                 for v1, v2 in self.arestas:
                     self.matriz_adjacencia[indice_v1][indice_v2] = 1
 
-    def mostrar_matriz_adjacencia(self):
+    def mostrar_matriz_adjacencia(self):    # Método para exibir a matriz de adjacência
         print("Matriz de Adjacência:")
         print("Tipo do Grafo:", self.tipo_grafo)
         for linha in self.matriz_adjacencia:
@@ -37,9 +36,9 @@ class Grafo:
         print("Após leitura de 'meu_arquivo.txt' a matriz representada acima:")
 
 
-class MinhaApi: # formtado do arquivo gerado: graph {1 -- 2;1 -- 3;2 -- 4;3 -- 5;4 -- 5;}
+class MinhaApi:
     @staticmethod
-    def gerar_arq_layout_nao_direcionado(arestas):
+    def gerar_arq_layout_nao_direcionado(arestas):  # Formato do arquivo gerado: graph {1 -- 2;1 -- 3;2 -- 4;3 -- 5;4 -- 5;}
         dados_grafico = 'graph {'
         for v1, v2 in arestas:
             dados_grafico += f"{v1} -- {v2};"
@@ -49,7 +48,7 @@ class MinhaApi: # formtado do arquivo gerado: graph {1 -- 2;1 -- 3;2 -- 4;3 -- 5
             file.write(dados_completos)
 
     @staticmethod
-    def gerar_arq_layout_direcionado(arestas):
+    def gerar_arq_layout_direcionado(arestas):  # Formato do arquivo gerado: digraph {1 -> 2;3 -> 1;2 -> 4;3 -> 5;4 -> 5;}
         dados_grafico = "digraph {"
         for v1, v2 in arestas:
             dados_grafico += f"{v1} -> {v2};"
@@ -65,7 +64,7 @@ class MinhaApi: # formtado do arquivo gerado: graph {1 -- 2;1 -- 3;2 -- 4;3 -- 5
             return parametro_api
 
 
-class OperacoesGrafo: # Classe que irá fazer as operações com os grafos
+class OperacoesGrafo:   # Classe que irá fazer as operações com os grafos
     @staticmethod
     def sair():
         print("Saindo do programa...")
@@ -75,18 +74,18 @@ class OperacoesGrafo: # Classe que irá fazer as operações com os grafos
         print("Opção inválida. Escolha uma opção válida.")
 
     @staticmethod
-    def busca_adjacente_nao_direcionado(vertice1, vertice2, arestas):
+    def busca_adjacente_nao_direcionado(vertice1, vertice2, arestas):   # Verifica se vértices são adjacentes ND
         return [vertice1, vertice2] in arestas or [vertice2, vertice1] in arestas
 
     @staticmethod
-    def busca_adjacente_direcionado(vertice1, vertice2, arestas):
+    def busca_adjacente_direcionado(vertice1, vertice2, arestas):   # # Verifica se vértices são adjacentes D
         for v1, v2 in arestas:
             if (v1 == vertice1 and v2 == vertice2) or (v1 == vertice2 and v2 == vertice1):
                 return True
         return False
 
     @staticmethod
-    def calcula_grau(vertice1, vertices, matriz_adjacencia):
+    def calcula_grau(vertice1, vertices, matriz_adjacencia):    # Verifica a qtd de 1 na linha para retornar o grau
         if vertice1 in vertices:
             indice_vertice = vertices.index(vertice1)
             grau = sum(matriz_adjacencia[indice_vertice])
@@ -98,8 +97,8 @@ class OperacoesGrafo: # Classe que irá fazer as operações com os grafos
         for a in arestas:
             if vertice1 in a:
                 vizinhos.update(a)
-        vizinhos.remove(vertice1)  # Remover o próprio vértice da lista de vizinhos
-        return sorted(vizinhos)
+        vizinhos.remove(vertice1)  # Remove o próprio vértice da lista de vizinhos
+        return sorted(vizinhos) # Função para ordenar
 
     @staticmethod
     def busca_vizinhos_direcionado(vertice1, arestas):
@@ -110,7 +109,7 @@ class OperacoesGrafo: # Classe que irá fazer as operações com os grafos
         return vizinhos
 
     @staticmethod
-    def detecta_arvore(grafo):
+    def detecta_arvore(grafo):      # Método para detectar se o grafo é uma árvore
         if grafo.tipo_grafo == 'D':
             print("Um grafo direcionado não pode ser uma árvore.")
             return False
@@ -119,8 +118,8 @@ class OperacoesGrafo: # Classe que irá fazer as operações com os grafos
         fila = list(grafo.vertices)[0:1]  # Começamos pela raiz
 
         while fila:
-            vertice = fila.pop(0)
-            if vertice in visitados:
+            vertice = fila.pop(0) # Método pop() remove o elemento da fila
+            if vertice in visitados: # Se o vértice já tiver sido visitado indica presença de ciclo
                 print("O grafo possui ciclos e, portanto, não é uma árvore.")
                 return False
             visitados.add(vertice)
@@ -174,7 +173,6 @@ class Menu: # Classe Menu
     def exibir_menu(self):
         try:
             while True:
-                print("### MENU ###")
                 print("1. Descobrir se dois vértices são adjacentes: ")
                 print("2. Calcular o grau de um vértice informado: ")
                 print("3. Buscar vizinho do vértice informado: ")
@@ -199,18 +197,23 @@ class Menu: # Classe Menu
                             verifica_adjacente_direcionado = OperacoesGrafo.busca_adjacente_direcionado(vertice1, vertice2, self.grafo.arestas)
                             if verifica_adjacente_direcionado:
                                 print(f"Os vértices {vertice1} e {vertice2} são adjacentes.")
+                                input("Pressione <enter> para continuar")
                             else:
                                 print(f"Os vértices {vertice1} e {vertice2} não são adjacentes.")
+                                input("Pressione <enter> para continuar")
                         else:
                             print("Entrada para tipo de grafo não encontrada !!!")
+                            input("Pressione <enter> para continuar")
                     else:
                         print("Um dos vértices NÃO faz parte do grafo !!!")
+                        input("Pressione <enter> para continuar")
 
 
                 elif opcao == "2":
                     vertice1 = str(input("Informe o vértice que deseja descobrir o grau: "))
                     calcula_grau = OperacoesGrafo.calcula_grau(vertice1, self.grafo.vertices, self.grafo.matriz_adjacencia)
                     print("GRAU DO VÉRTICE", vertice1, ":", calcula_grau)
+                    input("Pressione <enter> para continuar")
 
 
                 elif opcao == "3":
@@ -228,8 +231,10 @@ class Menu: # Classe Menu
                 elif opcao == "4":
                     if self.grafo.tipo_grafo == "ND":
                         OperacoesGrafo.visitar_arestas(self.grafo.vertices, self.grafo.arestas, direcionado=False)
+                        input("Pressione <enter> para continuar")
                     elif self.grafo.tipo_grafo == "D":
                         OperacoesGrafo.visitar_arestas(self.grafo.vertices, self.grafo.arestas, direcionado=True)
+                        input("Pressione <enter> para continuar")
 
 
                 elif opcao == "5":
@@ -246,6 +251,7 @@ class Menu: # Classe Menu
                                 with open("img_grafo_não_digrafo.svg", "w") as f:
                                     f.write(response.text)
                                 print("Gráfico gerado com sucesso como img_grafo_não_digrafo.svg")
+                                input("Pressione <enter> para continuar")
                             else:
                                 print("Erro na requisição:", response.status_code)
                                 print("Conteúdo da resposta:", response.text)
@@ -264,6 +270,7 @@ class Menu: # Classe Menu
                             with open("img_grafo_digrafo.svg", "w") as f:    # Salvando o SVG retornado pela API em um arquivo
                                 f.write(response.text)
                             print("Gráfico gerado com sucesso como img_grafo_digrafo.svg")
+                            input("Pressione <enter> para continuar")
                         else:
                             print(f"Erro ao gerar o gráfico: {response.text}")
 
@@ -271,6 +278,7 @@ class Menu: # Classe Menu
                 elif opcao == "6":
                     if self.grafo:
                         OperacoesGrafo.detecta_arvore(self.grafo)
+                        input("Pressione <enter> para continuar")
                     else:
                         print("Grafo não inicializado. Por favor, leia o arquivo primeiro.")
 
@@ -286,7 +294,7 @@ class Menu: # Classe Menu
             print("\n\nOperação interrompida pelo usuário.")
 
 
-if __name__ == "__main__":  # Inicia o programa
+if __name__ == "__main__":  # Começa o programa
     meu_arquivo = "meu_arquivo.txt"
     grafo = Grafo(meu_arquivo)
     grafo.ler_arquivo_padrao()
